6c6a454ffefc877e9f4880e79a8deeede2dd70e1
Haojen <haojenma@gmail.com>
2022-11-01 22:31:54 +0800

    Ready to add Privacy Policy

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,27 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import HomeView from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/help',
+        name: 'support',
+        component: () => import('../views/Support.vue')
+    },
+    {
+        path: '/privacy',
+        name: 'policy',
+        component: () => import('../views/PrivacyPolicy.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
