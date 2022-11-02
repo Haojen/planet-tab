04da9abb2e94e9eacdb7a64921c190e10ed7e843
Haojen <haojenma@gmail.com>
2022-11-02 18:09:24 +0000

    Deploying to gh-pages from  @ b298587d5f0643d5054fa6e2fbe362f0bf7f1a0d 🚀

--- vue.config.js
+++
@@ -1,5 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/planet-tab/' : '/'
-})
